e792f0280f713c4beef1afd168c1ebcbd9cd7e72
Anshika Soni <anshikasoni357@gmail.com>
2024-05-18 23:51:44 +0530

    Macro planner

---
+++ src/main/java/com/macroplanner/MacroPlanner/MacroPlannerApplication.java
@@ -0,0 +1,14 @@
+package com.macroplanner.MacroPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // this annotation says this is a spring boot application
+public class MacroPlannerApplication {
+
+	// this is the main method responsible for running our project
+	public static void main(String[] args) {
+		SpringApplication.run(MacroPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macroplanner/MacroPlanner/MacroPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macroplanner.MacroPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
