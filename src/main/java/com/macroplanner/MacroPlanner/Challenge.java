c4c3b375a9042abcb533895d42e51680b7328621
Anshika Soni <anshikasoni357@gmail.com>
2024-05-19 15:43:01 +0530

    constructing the foundation

---
+++ src/main/java/com/macroplanner/MacroPlanner/Challenge.java
@@ -0,0 +1,14 @@
+package com.macroplanner.MacroPlanner;
+
+public class Challenge {
+    private Long id;
+    private String month;
+    private String description;
+
+    public Challenge(Long id, String month, String description) {
+        this.id = id;
+        this.month = month;
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/com/macroplanner/MacroPlanner/PlannerControll.java
@@ -0,0 +1,8 @@
+package com.macroplanner.MacroPlanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlannerControll {
+    private List<Challenge> challenges = new ArrayList<>();
+}
