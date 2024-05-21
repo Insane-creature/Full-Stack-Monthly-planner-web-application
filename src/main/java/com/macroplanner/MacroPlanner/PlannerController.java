1cf265e925ada3bc4863aa5e82701742acfd6d88
Anshika Soni <anshikasoni357@gmail.com>
2024-05-22 03:25:29 +0530

    Added put and delete in app and modified code using RequestMapping by Springboot

--- src/main/java/com/macroplanner/MacroPlanner/PlannerController.java
+++ src/main/java/com/macroplanner/MacroPlanner/PlannerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,4 +47,14 @@ public class PlannerController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @PutMapping("/challenges/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id, @RequestBody Challenge updatedChallenge) {
+        boolean isChallengeUpdated = challengeServices.updateChallenge(id, updatedChallenge);
+        if (isChallengeUpdated) {
+            return new ResponseEntity<>("Challenge updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Challenge not updated successfully", HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
